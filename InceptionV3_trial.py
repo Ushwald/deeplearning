5aeb6e0c7275c31530991b831aec9c6f6f21d291
Ryan Oloughlin <rmoloughlin11@gmail.com>
2021-02-19 15:26:28 +0100

    Added truncated dataset and collaborative work from liveshare

---
+++ InceptionV3_trial.py
@@ -0,0 +1,104 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import os
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print("Deleted %d images" % num_skipped)
+image_size = (180, 180)
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+
+print(train_ds.class_names)
+
+
+augmented_train_ds = train_ds.map(
+  lambda x, y: (x, y))
+
+train_ds = train_ds.prefetch(buffer_size=32)
+val_ds = val_ds.prefetch(buffer_size=32)
+
+model = tf.keras.applications.InceptionV3(
+    include_top=True,
+    weights=None,
+    input_tensor=None,
+    input_shape=image_size + (3,),
+    pooling=None,
+    classes=(2),
+    classifier_activation="softmax",
+)
+
+
+#make_model(input_shape=image_size + (3,), num_classes=2)
+#keras.utils.plot_model(model, show_shapes=True)
+
+epochs = 10
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+
+img = keras.preprocessing.image.load_img(
+    "PetImages/Cat/10009.jpg", target_size=image_size
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+score = predictions[0]
+print(
+    "This image is %.2f percent cat and %.2f percent dog."
+    % (100 * (1 - score), 100 * score)
+)
+
